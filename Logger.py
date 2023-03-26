0f0313b91de3051089469880a211961e76853a85
SickerinoDino <valentinschmidt01@t-online.de>
2023-03-26 13:15:12 +0200

    Anpassung des CSV Datei Paths

--- Logger.py
+++ Logger.py
@@ -4,7 +4,7 @@ import time
 
 def WriteInLog(loglist):
     zeit = time.asctime().replace(" ","_")
-    with open(r'C:\Users\valen\OneDrive\Dokumente\Berufsschule\LF8\LOGGER.csv', 'a', newline='') as csvdatei:
+    with open(r'LOGGER.csv', 'a', newline='') as csvdatei:
         writer = csv.writer(csvdatei)
         writer.writerow([zeit,loglist])
     
