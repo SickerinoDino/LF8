61f4313050b857151d985cc24f80c561f987790d
SickerinoDino <126165488+SickerinoDino@users.noreply.github.com>
2023-03-27 10:58:06 +0200

    Update LF8.py
    Signed-off-by: SickerinoDino <126165488+SickerinoDino@users.noreply.github.com>

--- LF8.py
+++ LF8.py
@@ -3,7 +3,7 @@ import tkinter as tk
 import Logger 
 
 
-#Liste für LOG Datei wie Variable schon sagt du Idiot
+#Liste für LOG Datei
 loglist = []
 
 #Erstellen des Hauptfensters und Festlegen des Titels und der Größe
@@ -39,21 +39,21 @@ def fehlermeldung(fcode, fehler):
 def update_cpu():
     cpu_perc = cpu_percent()
     cpu_label.config(text=f"CPU-Auslastung: {cpu_perc}%")
-    root.after(1000, update_cpu)
     if cpu_perc >= 90:
         fehlermeldung("CPU", cpu_perc)
     loglist.append(cpu_perc)
-
+    root.after(1000, update_cpu)
+    
 # refreshed die ausgegebenen Memory-Daten aller 1000ms 
 def update_ram():
     memory_usage = virtual_memory().used
     memory = memory_usage/1024/1024/1024
     memory_perc = virtual_memory().percent
     ram_label.config(text=f"Arbeitsspeicher-Auslastung: {round(memory,3)} GiB")
-    root.after(1000, update_ram)
     if memory_perc >= 90:
         fehlermeldung("RAM", memory_perc)
     loglist.append(memory_perc)
+    root.after(1000, update_ram)
 
 # refreshed die ausgegebenen Festplatten-Daten aller 1000ms 
 def update_disk():
@@ -63,22 +63,22 @@ def update_disk():
         disk_total = disk_usage(disk_par[i][0]).total/1024/1024/1024
         disk_perc = disk_usage(disk_par[i][0]).percent
         disk_label.config(text=f"{disk_par[i][0]} {round(disk_use,3)} GiB frei von {round(disk_total,3)} GiB: {disk_perc}%")
-    root.after(1000, update_disk)
-    if disk_perc >= 90 :
-        fehlermeldung("Disk", disk_perc)
+        if disk_perc >= 90 :
+            fehlermeldung("Disk", disk_perc)
+    root.after(1000, update_disk
 
 # refreshed die ausgegebenen Network-Daten aller 1000ms 
 def update_network():
     network_io_counters = net_io_counters()
     network_usage = (network_io_counters.bytes_sent + network_io_counters.bytes_recv)/1024/1024/1024
     network_label.config(text=f"Netzwerkauslastung: {round(network_usage,3)} GB")
-    root.after(1000, update_network)
     if network_io_counters.bytes_recv == 0:
         fehlermeldung("Netzwerk", 0)
     loglist.append(network_usage)
     Logger.WriteInLog(loglist)
     loglist.clear()
-
+    root.after(1000, update_network)
+               
 #Erstellen der Labels für die verschiedenen Systeminformationent und Verbindung mit dem Root-Fenster.
 cpu_label = tk.Label(root, text="CPU-Auslastung: ")
 cpu_label.pack()
